c4cc66406a73949e23965896a330d0f33b5bb110
Shaun Tucker <tucke@shauns-air.home>
2018-02-18 15:45:05 -0500

    Model

---
+++ play3/Models/Models.swift
@@ -0,0 +1,26 @@
+//
+//  Models.swift
+//  play3
+//
+//  Created by Shaun Tucker on 2/18/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class AppCategory: NSObject {
+    
+    var name: String?
+    
+    var app: [App]?
+}
+
+class App: NSObject {
+    
+    var id: NSNumber?
+    var name: String?
+    var category: String?
+    var imageName: String?
+    var price: NSNumber?
+    
+}
