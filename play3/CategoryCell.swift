718efcad647c2e5a566986ccbfb8b8753f992cdd
Shaun Tucker <tucke@shauns-air.home>
2018-02-18 09:36:18 -0500

    Created a CategoryCell class

---
+++ play3/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  play3
+//
+//  Created by Shaun Tucker on 2/18/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.black
+    }
+    
+}
--- play3/FeaturedAppController.swift
+++ play3/FeaturedAppController.swift
@@ -35,23 +35,7 @@ class FeaturedAppController: UICollectionViewController, UICollectionViewDelegat
 
 }
 
-class CategoryCell: UICollectionViewCell {
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupViews() {
-        backgroundColor = UIColor.red
-    }
-    
-}
+
     
     
 
