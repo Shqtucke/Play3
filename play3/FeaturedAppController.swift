edd82cf5333d5c79b3598f45bb08b3c0a58f1f65
Shaun Tucker <tucke@shauns-air.home>
2018-02-18 09:15:15 -0500

    Red ViewController

---
+++ play3/FeaturedAppController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  play3
+//
+//  Created by Shaun Tucker on 2/18/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedAppController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.red
+    }
+
+  
+
+
+}
+
