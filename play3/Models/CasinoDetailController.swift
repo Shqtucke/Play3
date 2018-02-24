33611dfb5723c2ed0ec0393748ece7511e529334
Shaun Tucker <tucke@shauns-air.home>
2018-02-24 15:47:32 -0500

    segue performed successfully

--- play3/Models/CasinoDetailController.swift
+++ play3/Models/CasinoDetailController.swift
@@ -9,12 +9,67 @@
 import Foundation
 import UIKit
 
-class CasinoDetailController: UICollectionViewController {
+class CasinoDetailController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let headerId = "headerId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = UIColor.red
+        collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(CasinoDetailHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! CasinoDetailHeader
+        
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 170)
+    }
+}
+
+class CasinoDetailHeader: BaseCell {
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleToFill
+        return iv
+        
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        backgroundColor = UIColor.blue
+       // addSubview(imageView)
+    }
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
         
     }
 }
+
+
+
+
+
+
+
+
+
